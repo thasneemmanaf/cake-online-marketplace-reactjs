5a971c128352eadcc357e1535e8c869830813cf3
thasneemmanaf <123thasneemsalim@gmail.com>
2020-11-14 11:24:06 +0100

    Add Products and its styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Hero from './components/Hero';
 import GlobalStyle from './GlobalStyles';
+import Products from './components/Products';
+import { productData } from './components/Products/data';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading="Choose your favorite" data={productData} />
     </Router>
   );
 }
