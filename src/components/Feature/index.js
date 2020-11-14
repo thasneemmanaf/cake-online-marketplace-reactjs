06f0429b12943d65438982304184fe411a5e7767
thasneemmanaf <123thasneemsalim@gmail.com>
2020-11-14 11:52:58 +0100

    Add Featured section and styling

---
+++ src/components/Feature/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FeatureContainer, FeatureButton } from './style';
+
+function Feature() {
+  return (
+    <FeatureContainer>
+      <h1>Cake of the Day</h1>
+      <p>Truffle alfredo sauce topped with 24 carat gold dust.</p>
+      <FeatureButton>Order Now</FeatureButton>
+    </FeatureContainer>
+  );
+}
+
+export default Feature;
